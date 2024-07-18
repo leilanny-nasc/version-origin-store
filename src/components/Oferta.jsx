18e4812cf49294da3fc0a9e606f24ac7eb476120
Leilanny <leilannynascimento19@gmail.com>
2024-07-18 16:34:55 -0300

    new components

---
+++ src/components/Oferta.jsx
@@ -0,0 +1,26 @@
+
+const Oferta = () => {
+  return (
+    <div className="ofertaPage">
+      <div className="ci-oferta"></div>
+      <div className="circulo-oferta"></div>
+
+      <div className="imagem-oferta">
+      </div>
+
+    <div className="texto-oferta">
+        <div className="texto01">Oferta Especial</div>
+        <div className="texto02">Air Jordan edição de colecionador</div>
+        <div className="texto03">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse alias
+          rerum placeat tempora dolor voluptas. Fuga magnam iure, saepe vero
+          labore qui expedita velit! Accusantium quisquam molestiae eum laborum
+          fuga!
+        <div><button>Ver Oferta</button></div>
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default Oferta;
