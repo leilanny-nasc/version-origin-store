4e73ddcbcc0be57d0d2ef20638ad897aec18af55
Leilanny <leilannynascimento19@gmail.com>
2024-07-23 19:52:18 -0300

    new components

---
+++ src/components/ProdutosAlta.jsx
@@ -0,0 +1,96 @@
+import tenis from '../assets/tenis.png'
+
+const ProdutosAlta = () => {
+    return ( 
+        <>
+        <section> 
+            <p>Produtos em alta</p>
+            <a href="/">Ver todos</a>
+
+            <ul>
+                <li className="card">
+                    <div className="card-produto" >
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card"/>
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li className='card'>
+                    <div className="">
+                        <img src={tenis} alt="" className="tenis-card" />
+                        <h6>Tênis</h6>
+                        <h3>K-Swiss V8 - Masculino</h3>
+                        <p><s>$200</s> $100</p>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+        </>
+     );
+}
+ 
+export default ProdutosAlta;
